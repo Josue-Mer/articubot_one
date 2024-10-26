badd7ddf2e62f720a81f3415b6f10ed8f80edef3
Josue-Mer <pavoro30@gmail.com>
2024-10-25 22:56:34 -0400

    control servo with controller button, also configured to python package

--- setup.py
+++ setup.py
@@ -8,25 +8,27 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    py_modules=[
-        # 'articubot_one.laser_scan_filter'
-    ],
+    package_dir={'': 'src'},
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-    ],
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'config'), glob('config/*')),
+    (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    (os.path.join('share', package_name, 'src'), glob('src/articubot_one/*.py')),
+    (os.path.join('share', package_name, 'description'), glob('description/*')),
+    (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+],
+
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='Package description',
-    license='License declaration',
+    maintainer='Josh Newans',
+    maintainer_email='my_email@email.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # 'laser_scan_filter = articubot_one.laser_scan_filter:main',
+            'joy_button_action = articubot_one.joy_button_action:main',
         ],
     },
 )
